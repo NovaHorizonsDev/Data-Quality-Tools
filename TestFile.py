828afa479b884bbe29621188133dc97f5f172fdf
Trevor <113529065+NovaHorizonsDev@users.noreply.github.com>
2025-03-11 21:24:35 -0600

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -2,9 +2,3 @@
 from DataManipulation import MissingElements
 
 
-ME= MissingElements('C:/Users/trevo/PycharmProjects/DataQualityTools/uncleaned_data.csv')
-
-rows = ME.findMissingElements()
-
-
-ME.deleteEmptyElementRows()
